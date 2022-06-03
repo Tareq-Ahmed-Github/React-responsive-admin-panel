22756344028a0ac99aa2233f640eba944748aa90
unknown <tareqrobin57@gmail.com>
2022-06-04 00:59:19 +0600

    sidebar menu

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './../../images/logo.png';
 import './Sidebar.css';
+import { SidebarData } from '../../Data/Data';
+import {UilSignOutAlt} from '@iconscout/react-unicons';
 const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0);
+    
+
     return (
         <div className="Sidebar">
             <div className='logo'>
                 <img src={logo} alt="" />
                 <span>Sh<span>o</span>p</span>
+            </div>
+
+            {/* menu  */}
+            <div className="menu">
+                {
+                    SidebarData.map((item,index)=>{
+                        return(
+                            <div className="menuItem">
+                                <item.icon/>
+                                <span>
+                                    {item.heading}
+                                </span>
+                            </div>
+                        )
+                    })
+                }
+                <div className="menuItem">
+                        <UilSignOutAlt/>
+                    </div>
             </div>
         </div>
     );
